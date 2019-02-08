fe1d73bba37f8946807bcaf4b3874b6e1edb07d5
Nick Simmons <nicksimmons53@gmail.com>
2019-02-08 13:21:37 -0600

    CS3733 - assign1

--- cs3733/assign/assign1/wordcount.c
+++ cs3733/assign/assign1/wordcount.c
@@ -1,12 +1,17 @@
-#include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <errno.h>
+#include <sys/wait.h>
+
+int wordCount(char *fileName);
 
 int main(int argc, char *argv[ ]) {
 	int numOfFiles;
 	int i;
 	char *fileNames[argc];
+	pid_t childPID = 0;
+	long parentPID = getpid( );
 
 	// Usage Error, exit if true
 	if (argc <= 1) {
@@ -23,9 +28,50 @@ int main(int argc, char *argv[ ]) {
 
 	// Main process creation for each file
 	for (i = 0; i < numOfFiles; i++) {
+		int count;
 
+		// Create child process for counting words
+		if ((childPID = fork( )) <= 0)
+			break;
+
+		// Call to wordCount function
+		count = wordCount(fileNames[i]);
+		printf("Child process for %s: number of words is %d\n", fileNames[i], count);
 	}
+
+	// for loop for parent process to wait for child completion
+	for ( ; ; ) {
+		childPID = wait(NULL);
+		if (childPID == -1 && errno != EINTR)
+			break;
+	}
+
+	// If current process is parent process, complete the program
+	if (parentPID == getpid( ))
+		printf("All %d files have been counted!\n", numOfFiles);
 
 	fflush(stderr);
 	return 0;
+}
+
+// int wordCount(char *fileName)
+//
+// Task: 		wordCount will open a file and read it line by line and return the total count of
+//				words in the file
+// Parameter:	char *fileName		file name to be read
+// Return:		int 				amount of words in the file
+int wordCount(char *fileName) {
+	FILE *file;
+	char c;
+	int wordCount = 0;
+	int i;
+
+	file = fopen(fileName, "r");
+	while ((c = fgetc(file)) != EOF) {
+		if (c == ' ' || c == '\n' || c == '\t')
+			wordCount++;
+	}
+
+	fclose(file);
+	return wordCount;
 }
